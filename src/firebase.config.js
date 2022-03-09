aed7c506ea78918d6e24d9a0dd1194da2c1a4ddb
Sabrie Sevil Hatipoglu <sevilhatipogluu93@gmail.com>
2022-03-09 17:34:09 +0300

    installed firecase

---
+++ src/firebase.config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyChqXlHlt0xGRgdEPiTvl0w9nZlKKvQFfU",
+  authDomain: "house-marketplace-5b75d.firebaseapp.com",
+  projectId: "house-marketplace-5b75d",
+  storageBucket: "house-marketplace-5b75d.appspot.com",
+  messagingSenderId: "272206180",
+  appId: "1:272206180:web:c1047b5dd09ae65f451271"
+};
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore();
