8b0851d7813bcf30db14586ab0ee82f38dca2ba4
Sabrie Sevil Hatipoglu <sevilhatipogluu93@gmail.com>
2022-03-09 19:38:35 +0300

    added routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Explore from "./pages/Explore";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div>Hello</div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<SignIn />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+        <Navbar />
+      </Router>
+    </>
   );
 }
 
